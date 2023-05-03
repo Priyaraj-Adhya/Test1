51e7f0e4cabf6b9c378f2024d314923b72411381
Priyaraj Adhya <126314115+Priyaraj-Adhya@users.noreply.github.com>
2023-05-04 00:26:15 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 provider "aws" {
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "Demo-terraform" {
+resource "aws_instance" "Demo" {
     ami = "ami-06c2ec1ceac22e8d6"
     instance_type = "t2.micro"
 }
