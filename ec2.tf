1c297926e4ee50b7446eef3208a81264aab3ecce
Priyaraj Adhya <126314115+Priyaraj-Adhya@users.noreply.github.com>
2023-05-04 00:41:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "Demo" {
-    ami = "ami-06c2ec1ceac22e8d6"
-    instance_type = "t2.micro"
+resource "aws_eip" "lb" {
+  instance = aws_instance.web.id
+  vpc      = true
 }
